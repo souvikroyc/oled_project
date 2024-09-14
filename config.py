f8e3fc0a3c6232ed6ec99db71c9d2342f89337e9
souvikroyc <53352249+souvikroyc@users.noreply.github.com>
2024-09-14 22:55:57 +0200

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 
 # OpenWeatherMap API setup
-API_KEY = "11620ab2b355b6c9437652b4a27e19a9"  # Replace with your API key
-CITY = "Ljubljana"                       # Replace with your city
+API_KEY = "your_openweathermap_api_key"  # Replace with your API key
+CITY = "your_city"                       # Replace with your city
