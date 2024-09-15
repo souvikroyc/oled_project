0887769a3fb624f99bcb5d05291d236a9fa71892
souvikroyc <53352249+souvikroyc@users.noreply.github.com>
2024-09-15 12:04:29 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@
 # OpenWeatherMap API setup
 API_KEY = "your_openweathermap_api_key"  # Replace with your API key
 CITY = "your_city"                       # Replace with your city
+# Path to the icons folder in your project
+icons_path = "./icons/"  # Assuming the 'icons' folder is in the root directory of the GitHub project
