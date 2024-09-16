a0d316b8afaa0d92d2740744d4c250cf42adf1ff
souvikroyc <53352249+souvikroyc@users.noreply.github.com>
2024-09-17 00:34:55 +0200

    Update config.py
    country code

--- config.py
+++ config.py
@@ -2,6 +2,9 @@
 
 # OpenWeatherMap API setup
 API_KEY = "your_openweathermap_api_key"  # Replace with your API key
-CITY = "your_city"                       # Replace with your city
-# Path to the icons folder in your project
-icons_path = "./icons/"  # Assuming the 'icons' folder is in the root directory of the GitHub project
+CITY = "your_city"  # Replace with your city
+COUNTRY = "your country"  # Country code for your country
+WEATHER_URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+# Path to the icons folder
+icons_path = "./icons/"  # Relative path, assuming the icons folder is in the same directory as config.py
+
